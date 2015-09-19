8f9982e70036ab6566c6eef21ded5dec8354e729
Helio Ribeiro <helio_r_cruz@hotmail.com>
2015-09-19 19:41:57 -0300

    adding java client

---
+++ java_client/src/br/com/app/client/Espec.java
@@ -0,0 +1,30 @@
+package br.com.app.client;
+
+public class Espec {
+
+	private Gender gender;
+	private String studio;
+
+	public Espec(Gender gender, String studio) {
+		this.gender = gender;
+		this.studio = studio;
+	}
+
+	public Gender getGender() {
+		return gender;
+	}
+
+	public String getStudio() {
+		return studio;
+	}
+
+	public boolean match(Espec espMovie){
+		if(gender.equals(espMovie.gender) && studio.equals(espMovie.studio)) return true;
+		return false;
+	}
+
+	public String toString(){
+		return gender+" | "+studio;
+	}
+
+}
